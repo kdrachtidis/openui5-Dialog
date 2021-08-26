b1b9a785f107f3d889b6a424d651ccab1c932d7e
Konstantinos Drachtidis <konstantinos.drachtidis@sap.com>
2021-08-26 12:14:14 +0200

    Init

--- controller/MainNavigation/MainPage.controller.js
+++ controller/MainNavigation/MainPage.controller.js
@@ -1,9 +1,39 @@
 sap.ui.define([
-    'sap/ui/core/mvc/Controller'
-], function (Controller) {
+    "sap/m/Button",
+    "sap/m/Dialog",
+    "sap/m/Text",
+    "sap/m/library",
+    "sap/ui/core/mvc/Controller"
+], function (Button, Dialog, Text, mobileLibrary, Controller) {
     "use strict";
+    
+    var ButtonType = mobileLibrary.ButtonType;
 
-    var Controller = Controller.extend("DemoUXapp.controller.MainNavigation.MainPage");
+    var Controller = Controller.extend("DemoUXapp.controller.MainNavigation.MainPage", {
+        onMessageDialogPress: function () {
+            var oDialog = new Dialog({
+                title: 'Default Message',
+                contentWidth: '400px',
+                type: 'Message',
+                content: new Text({
+                    text: 'Fix size'
+                }),
+                beginButton: new Button({
+                    type: ButtonType.Emphasized,
+                    text: 'OK',
+                    press: function () {
+                        oDialog.close();
+                    }
+                }),
+                afterClose: function () {
+                    oDialog.destroy();
+                }
+            });
+
+            oDialog.open();
+        }
+    });
+
 
     return Controller;
 });
